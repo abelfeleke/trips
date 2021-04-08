87f4b356f4bcd5884e2a1acf9c91f53d18e20207
Michelle Kroll <michellekroll@berkeley.edu>
2021-04-08 10:34:11 -0700

    added sign in functionality

--- src/javascript/auth.js
+++ src/javascript/auth.js
@@ -10,16 +10,16 @@ signUp.addEventListener('submit', (e) => {
         // Here we get the modal that is opened up, in this case, the SignUp Modal
         // We then get that reference to that modal and using M (materialize library) we close it
         // Finally, we will reset the signup form so that the form clears up once a user signs up!
+         debugger;
          const modal = document.querySelector('#modal-signup');
-         M.Modal.getInstance(modal).close();
          signUp.reset();
     });
 });
     // Logout
-const logout = document.querySelector('#logout');
-logout.addEventListener('click', (e) => {
-        e.preventDefault();
-        auth.signOut().then(() => {
-        console.log('user signed out');
-    });
-});
+//const logout = document.querySelector('#logout');
+//logout.addEventListener('click', (e) => {
+      //  e.preventDefault();
+       // auth.signOut().then(() => {
+       // console.log('user signed out');
+   // });
+//});
