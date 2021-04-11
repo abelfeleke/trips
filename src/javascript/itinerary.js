9c95a9209e772288d483de846461fea572915e14
abel <afcal@berkeley.edu>
2021-04-11 01:35:47 +0000

    itinerary page works for logged in users

--- src/javascript/itinerary.js
+++ src/javascript/itinerary.js
@@ -1,7 +1,45 @@
+// html elements
 const events = document.querySelector('#event-list');
 const form = document.querySelector('#add-event-form');
 
-// create element & render event
+// trip info
+var tripId = localStorage.getItem("id_storage");
+var trip = db.collection("trips").doc(tripId); 
+
+// listens for new event and saves data
+form.addEventListener('submit', (e) => { 
+    e.preventDefault();
+    // add event to firestore 
+    db.collection('events').add({ 
+        name: form.name.value,
+        location: form.location.value,
+        details: form.details.value
+    }).then((event) => { 
+        // append event id to trip.events
+        trip.update({
+            events: firebase.firestore.FieldValue.arrayUnion(event.id) 
+        });
+    });
+    // clear form
+    form.reset() 
+});
+
+var eventsCollection = db.collection("events"); // events collection
+
+// real-time listener 
+trip.get().then((snapshot) => {
+    let eventIds = snapshot.data().events; 
+    // for each id 
+    eventIds.forEach((eventId) => { 
+        // get event
+        eventsCollection.doc(eventId).get().then((event) => { 
+            // display event
+            renderEvent(event); 
+        })
+    });
+});
+
+/* Helper fn: Create element & render event */
 function renderEvent(doc){
     let li = document.createElement('li');
     let name = document.createElement('span');
@@ -24,32 +62,12 @@ function renderEvent(doc){
 
     // deleting data
     cross.addEventListener('click', (e) => {
-        let id = e.target.parentElement.getAttribute('data-id'); 
-        db.collection('events').doc(id).delete();
+        let eventId = e.target.parentElement.getAttribute('data-id'); 
+        // delete event from firestore
+        eventsCollection.doc(eventId).delete(); 
+        // delete id from trip.events
+        trip.update({
+            events: firebase.firestore.FieldValue.arrayRemove(eventId) 
+        });
     });
 }
-
-// saving data
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    db.collection('events').add({
-        name: form.name.value,
-        location: form.location.value,
-        details: form.details.value
-    });
-    // Todo: Add Event ID to user.trips.events 
-    form.reset() // clear form
-});
-
-// real-time listener 
-db.collection('events').orderBy('name').onSnapshot(snapshot => {
-    let changes = snapshot.docChanges();
-    changes.forEach(change => {
-        if(change.type == 'added'){
-            renderEvent(change.doc);
-        } else if (change.type == 'removed'){
-            let li = events.querySelector('[data-id=' + change.doc.id + ']');
-            events.removeChild(li);
-        }
-    });
-});
