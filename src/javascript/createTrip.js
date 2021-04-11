5ac25eb1dc7fdf3ad53edf3f68da3756fbb41935
Maleny <malenyruiz123@gmail.com>
2021-04-11 10:29:29 -0700

    create trip, fixed issues and added functionality

--- src/javascript/createTrip.js
+++ src/javascript/createTrip.js
@@ -6,44 +6,62 @@ var docid;
 
 // wait for the sumbit button to be clicked and add the entries and doc id
 form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    db.collection('trips').add({
-        name: form.name.value,
-        location: form.location.value,
-        date_from: form.date_from.value,
-        date_to: form.date_to.value,
-        description: form.description.value,
-        events: []
-    })
-    .then((docRef) => {
+  e.preventDefault();
+  // check form
+  if (checkForm(form) ==  false) {
+    return;
+  }
+  db.collection('trips').add({
+    name: form.name.value,
+    location: form.location.value,
+    date_from: form.date_from.value,
+    date_to: form.date_to.value,
+    description: form.description.value,
+    events: []
+  })
+  .then((docRef) => {
     docid = docRef.id;
     var tripdoc = db.collection('trips').doc(docRef.id);
     var setWithMerge = tripdoc.update({
       code: docRef.id
     });
     // add trip to user's list
-    // get user id
-    // var userid = firebase.auth().currentUser;
-    // get user's list of trips to update them
-    // var user = db.collection('users').doc(userid);
     var user = db.collection('users').doc(userid);
     user.update({
-    trips: firebase.firestore.FieldValue.arrayUnion(docRef.id)
+      trips: firebase.firestore.FieldValue.arrayUnion(docRef.id)
     });
     //clear form
-    form.name.value = '';
-    form.location.value = '';
-    form.date_from.value = '';
-    form.date_to.value = '';
-    form.description.value = '';
-    console.log(docRef.id);
-    })
-    .then((value) => {
-      myFunction(value);
-    });
+    form.reset();
+  })
+  .then((value) => {
+    openWindow(value);
+  });
 });
 
+function openWindow(code) {
+  var myWindow = window.open("home.html", "_self");
+}
 
-function myFunction(code) {
-  var myWindow = window.open("itinerary.html", "_self");
+function checkForm(f) {
+  if (f.name.value == '') {
+    alert("Name must be filled out");
+    return false;
+  } else if (f.location.value == '') {
+    alert("Location must be filled out");
+    return false;
+  } else if (f.date_from.value == '') {
+    alert("Date from must be filled out");
+    return false;
+  } else if (f.date_to.value == '') {
+    alert("Date to must be filled out");
+    return false;
+  } else if (f.description.value == '') {
+    alert("Description must be filled out");
+    return false;
+  } else if (f.date_from.value > f.date_to.value) {
+    alert("Dates must be valid");
+    return false;
+  } else {
+    return true;
+  }
 }
