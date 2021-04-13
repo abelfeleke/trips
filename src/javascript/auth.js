6bf0341ee1326923a377c881640ca64ca3988e37
Michelle Kroll <michellekroll@berkeley.edu>
2021-04-12 17:40:06 -0700

    fixed login and need to fix css

--- src/javascript/auth.js
+++ src/javascript/auth.js
@@ -1,25 +1,44 @@
 //Sign up
 const signUp = document.querySelector('#signup-form');
-signUp.addEventListener('submit', (e) => {
-        e.preventDefault();
-        // Get user info
-        const email =  signUp['signup-email'].value;
-        const pw = signUp['signup-password'].value;
-         //Sign up a user
-        auth.createUserWithEmailAndPassword(email, pw).then(cred => {
-        // Here we get the modal that is opened up, in this case, the SignUp Modal
-        // We then get that reference to that modal and using M (materialize library) we close it
-        // Finally, we will reset the signup form so that the form clears up once a user signs up!
-         const modal = document.querySelector('#modal-signup');
-         M.Modal.getInstance(modal).close();
-         signUp.reset();
+if (signUp != null) {
+    signUp.addEventListener('submit', (e) => {
+            e.preventDefault();
+            // Get user info
+            const email =  signUp['signup-email'].value;
+            const pw = signUp['signup-password'].value;
+            //Sign up a user
+            auth.createUserWithEmailAndPassword(email, pw).then(cred => {
+            // Finally, we will reset the signup form so that the form clears up once a user signs up!
+            signUp.reset();
+        });
     });
-});
-    // Logout
-//const logout = document.querySelector('#logout');
-//logout.addEventListener('click', (e) => {
-      //  e.preventDefault();
-       // auth.signOut().then(() => {
-       // console.log('user signed out');
-   // });
-//});
+}
+
+//Login
+const loginForm = document.querySelector('#login-form');
+if (loginForm != null) {
+    loginForm.addEventListener('submit', (e) => {
+            e.preventDefault();
+            // get user info
+            const email = loginForm['login-email'].value;
+            const password = loginForm['login-password'].value;
+            // log the user in
+            auth.signInWithEmailAndPassword(email, password).then((cred) => {
+            window.location.href = '/src/html/home.html';
+            console.log(cred.user);
+            //reset form
+            loginForm.reset();
+    }).catch((incorrectpass) => {console.log(incorrectpass)});
+    });
+}
+
+//Logout
+const logout = document.querySelector('#logout');
+if (logout != null) {
+    logout.addEventListener('click', (e) => {
+        e.preventDefault();
+        auth.signOut().then(() => {
+        console.log('user signed out');
+    });
+    });
+}
